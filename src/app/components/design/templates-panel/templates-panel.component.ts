d82cc9952b89d7105adfc8ab3f3a27492c9bcc04
git-palace <edeveloper4@gmail.com>
2018-08-25 14:10:22 -0700

    updates

--- src/app/components/design/templates-panel/templates-panel.component.ts
+++ src/app/components/design/templates-panel/templates-panel.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./templates-panel.component.scss']
 })
 export class TemplatesPanelComponent implements OnInit {
+  element_title_arr: any = ['Banners', 'Text', 'Background', 'Photos', 'Box Layers', 'Listings', 'Grids', 'Widgets', 'Elements', 'Templates'];
+  selected_ele_idx: any = 1;
 
   constructor() { }
 
